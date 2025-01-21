55d0212c76f6ca659c667d6e722cb2f474d77853
coffeemania68 <gmschool0245@gmail.com>
2025-01-21 14:43:46 +0900

    Create imageUrls.js

---
+++ imageUrls.js
@@ -0,0 +1,35 @@
+export const imageUrls = {
+    // 앱 아이콘 & 공통
+    icons: {
+        favicon: "/images/icons/favicon.ico",
+        logo: "/images/icons/logo.png",
+        ogImage: "/images/common/og-image.jpg",  // 1200x630px
+        shareImage: "/images/common/share-image.jpg",  // 800x800px
+    },
+    
+    // 시즌1 썸네일 (각 600x600px)
+    season1: {
+        wonderland: "/images/thumbnails/s1/science-wonderland.jpg",
+        gumiho: "/images/thumbnails/s1/gumiho-school.jpg",
+        thor: "/images/thumbnails/s1/thor-lab.jpg",
+        // ... 나머지 시즌1 썸네일
+    },
+
+    // 시즌2 썸네일
+    season2: {
+        moonRabbit: "/images/thumbnails/s2/moon-science.jpg",
+        pinocchio: "/images/thumbnails/s2/pinocchio-robot.jpg",
+        dwarf: "/images/thumbnails/s2/dwarf-recycling.jpg",
+        // ... 나머지 시즌2 썸네일
+    },
+
+    // 챕터 이미지 (각 1200x800px)
+    chapters: {
+        wonderland: {
+            ch1: "/images/chapters/wonderland/chapter1.jpg",
+            ch2: "/images/chapters/wonderland/chapter2.jpg",
+            // ... 각 챕터별 이미지
+        },
+        // ... 다른 스토리들의 챕터 이미지
+    }
+};
