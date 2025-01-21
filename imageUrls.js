3634463994fa50b3a43b066712c1360a39aaac47
coffeemania68 <gmschool0245@gmail.com>
2025-01-21 15:17:36 +0900

    Update imageUrls.js

--- imageUrls.js
+++ imageUrls.js
@@ -1,34 +1,34 @@
 const imageUrls = {
-    mainBg: "./images/main-background.png",
-    mainIcon: "./images/magic-story-icon.png",
+    mainBg: "images/main-background.png",
+    mainIcon: "images/magic-story-icon.png",
     setThumbnails: {
         // 시즌 1
-        story1: "./images/story1.jpg",     // 이상한 나라의 과학 실험
-        story2: "./images/story2.jpg",     // 구미호의 신비한 학교생활
-        story3: "./images/story3.jpg",     // 토르의 번개 연구소
-        story4: "./images/story4.jpg",     // 인어공주의 해양 환경 교실
-        story5: "./images/story5.jpg",     // 도깨비 마을의 특별한 심부름
-        story6: "./images/story6.jpg",     // 프레이야의 마법 학교
-        story7: "./images/story7.jpg",     // 피터팬의 코딩 교실
-        story8: "./images/story8.jpg",     // 산신령님의 자연 탐험
-        story9: "./images/story9.jpg",     // 로키의 장난감 발명실
-        story10: "./images/story10.jpg",   // 선녀와 나무꾼의 에코하우스
-        story11: "./images/story11.jpg",   // 헨젤과 그레텔의 요리 실험실
-        story12: "./images/story12.jpg",   // 요르문간드의 해저 탐험
+        story1: "images/story1.jpg",     // 이상한 나라의 과학 실험
+        story2: "images/story2.jpg",     // 구미호의 신비한 학교생활
+        story3: "images/story3.jpg",     // 토르의 번개 연구소
+        story4: "images/story4.jpg",     // 인어공주의 해양 환경 교실
+        story5: "images/story5.jpg",     // 도깨비 마을의 특별한 심부름
+        story6: "images/story6.jpg",     // 프레이야의 마법 학교
+        story7: "images/story7.jpg",     // 피터팬의 코딩 교실
+        story8: "images/story8.jpg",     // 산신령님의 자연 탐험
+        story9: "images/story9.jpg",     // 로키의 장난감 발명실
+        story10: "images/story10.jpg",   // 선녀와 나무꾼의 에코하우스
+        story11: "images/story11.jpg",   // 헨젤과 그레텔의 요리 실험실
+        story12: "images/story12.jpg",   // 요르문간드의 해저 탐험
 
         // 시즌 2
-        story13: "./images/story13.jpg",   // 달토끼의 우주 과학교실
-        story14: "./images/story14.jpg",   // 피노키오의 로봇 공작소
-        story15: "./images/story15.jpg",   // 드워프의 재활용 공방
-        story16: "./images/story16.jpg",   // 저승사자의 착한 마음 일기
-        story17: "./images/story17.jpg",   // 발키리의 하늘 정원
-        story18: "./images/story18.jpg",   // 용궁에서 온 전학생
-        story19: "./images/story19.jpg",   // 신데렐라의 재활용 패션쇼
-        story20: "./images/story20.jpg",   // 오딘의 지혜 도서관
-        story21: "./images/story21.jpg",   // 라푼젤의 식물 관찰 일기
-        story22: "./images/story22.jpg",   // 방랑 호랑이의 한국 여행
-        story23: "./images/story23.jpg",   // 헤임달의 무지개 다리 여행
-        story24: "./images/story24.jpg"    // 잠자는 숲속의 생태 연구
+        story13: "images/story13.jpg",   // 달토끼의 우주 과학교실
+        story14: "images/story14.jpg",   // 피노키오의 로봇 공작소
+        story15: "images/story15.jpg",   // 드워프의 재활용 공방
+        story16: "images/story16.jpg",   // 저승사자의 착한 마음 일기
+        story17: "images/story17.jpg",   // 발키리의 하늘 정원
+        story18: "images/story18.jpg",   // 용궁에서 온 전학생
+        story19: "images/story19.jpg",   // 신데렐라의 재활용 패션쇼
+        story20: "images/story20.jpg",   // 오딘의 지혜 도서관
+        story21: "images/story21.jpg",   // 라푼젤의 식물 관찰 일기
+        story22: "images/story22.jpg",   // 방랑 호랑이의 한국 여행
+        story23: "images/story23.jpg",   // 헤임달의 무지개 다리 여행
+        story24: "images/story24.jpg"    // 잠자는 숲속의 생태 연구
     }
 };
 
